19eb65d353e1d9144abbed62d07e891ccbe87d07
feliperodrigs1 <feliperodrigsp@gmail.com>
2023-03-06 17:58:08 -0300

    initial structure for login

--- QuickDeveloper/Controllers/HomeController.cs
+++ QuickDeveloper/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace QuickDeveloper.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ QuickDeveloper/wwwroot/js/Login.js
@@ -0,0 +1,14 @@
+﻿const form = document.getElementById("login-form");
+
+form.addEventListener("submit", function (event) {
+    
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        alert("Por favor, preencha todos os campos!");
+    } else {
+        window.location.href = "Index.html";
+    }
+});
