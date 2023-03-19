75b09fb02539bb2d236e80f8917dfd8ee55740b0
Gabriel Rizzi <gabrielsrizzi@gmail.com>
2023-03-19 13:59:01 -0300

    1º - Model de conexão com o banco de dados criada; 2º - Metodo utilizando uma procedure já existente como exemplo; 3º - API completa de cadastro do usuário;

---
+++ QuickDeveloper/Models/Model_DB.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuickDeveloper.Models
+{
+    public class Model_DB
+    {
+        SqlConnection sqlConnection;
+
+        public Model_DB()
+        {
+            var builder = new ConfigurationBuilder()
+                       .SetBasePath(Directory.GetCurrentDirectory())
+                       .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+            IConfiguration _configuration = builder.Build();
+            sqlConnection = new SqlConnection(_configuration.GetConnectionString("Connection"));
+        }
+
+        public void Register_Requisition(int IdUser, int IdDev, int idRequisition, string Description)
+        {
+            sqlConnection.Open();
+
+            using (SqlCommand sqlCommand = new SqlCommand("spSLN_InsertUpdateRequisition", sqlConnection))
+            {
+
+                sqlCommand.Parameters.Clear();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.AddWithValue("@IDDEV", IdUser);
+                sqlCommand.Parameters.AddWithValue("@IDUSER", IdDev);
+                sqlCommand.Parameters.AddWithValue("@IDREQUISITION", idRequisition);
+                sqlCommand.Parameters.AddWithValue("@DESCRIPTION", Description);
+
+                var resultado = sqlCommand.ExecuteScalar();
+            }
+
+            sqlConnection.Close();
+         
+        }
+
+    }
+}
