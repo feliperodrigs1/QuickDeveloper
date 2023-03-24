aa4f7fa277e8b6e1faeeaa15f272f20b2afd7e41
Gabriel Rizzi <84163083+gabrielrrizzi@users.noreply.github.com>
2023-03-23 23:53:22 -0300

    [PSI - 17] - Adaptar API Login Usuário para o Projeto (#4)
    * 1º - Model de conexão com o banco de dados criada;
    2º - Metodo utilizando uma procedure já existente como exemplo;
    3º - API completa de cadastro do usuário;
    
    * Adicionado API
    
    * Acho que agora o usuário API sobe
    
    * testando alteração na Api de Usuario
    
    * Revert "testando alteração na Api de Usuario"
    
    This reverts commit 67c6de032c1d465c557f181d373a9d3b5b22dca8.
    
    * removendo API da solution
    
    ---------
    
    Co-authored-by: feliperodrigs1 <feliperodrigsp@gmail.com>

--- QuickDeveloper/Program.cs
+++ QuickDeveloper/Program.cs
@@ -1,3 +1,5 @@
+using QuickDeveloper.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -25,3 +27,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
