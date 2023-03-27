2c9201a169e0010ec68b40a61a802ed562e04cf9
Vinicius Rocha <vinicius-rochacruz@hotmail.com>
2023-03-26 22:11:05 -0300

    criada nova controller "Register" e nava model "User", ajustada distribuição de arquivos

---
+++ QuickDeveloper/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickDeveloper.Models;
+using System.Diagnostics;
+
+namespace QuickDeveloper.Controllers {
+    public class RegisterController : Controller {
+
+        Model_User usuario = new Model_User();
+        Model_DB dbase = new Model_DB();
+
+        public IActionResult Register() {
+            return View();
+        }
+
+
+        public IActionResult Competences(Model_User user) {
+            return View(usuario);
+        }
+
+
+        [System.Web.Mvc.HttpPost]
+        public IActionResult CompetencesPath() {
+            var path = Url.Action("Competences", "Home");
+            return Json(new { Path = path });
+        }
+
+        public IActionResult Login(Model_User user) {
+            usuario = user;
+            return View(usuario);
+        }
+
+        public IActionResult Cadastrar(Model_User user) {
+            usuario = user;
+            return RedirectToAction("Competences", "Register", usuario);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
