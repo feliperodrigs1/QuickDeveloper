6b5223bb932d6520d2bf78eeb4f8971872b6d596
Vinícius Rocha <42285855+viniciusrdc@users.noreply.github.com>
2023-04-03 16:29:02 -0300

    Adiciona controller destinada ao chatbot e acrescentada nova view para o chatbot (#8)
    Primeira versão do chatbot, ainda temos melhorias a serem feitas. Estou apenas aprovando para que todos tenham acesso a codificação do Vini.

---
+++ QuickDeveloper/Controllers/ChatController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickDeveloper.Models;
+
+namespace QuickDeveloper.Controllers {
+    public class ChatController : Controller {
+        public IActionResult ChatBot() {
+            return View();
+        }
+    }
+}
---
+++ QuickDeveloper/wwwroot/js/ChatBot.js
@@ -0,0 +1,19 @@
+﻿$(document).keypress(function (e) {
+    if (e.which == 13) $('#Send').click();
+});
+
+$(function SendMenssage() {
+    $("#Send").click(
+        function () {
+            var menssage = $("#MenssageTxt").val();
+
+            if (menssage != "") { 
+                $("#Chatbot").append("Eu: " + menssage + "\n")
+                $("#Chatbot").append("ChatBot: " + "Ainda estamos em fase de testes!"+"\n");
+                $("#MenssageTxt").val("");
+            }
+
+        }
+    );
+});
+
