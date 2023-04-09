fba4ab2596fedb337c7b49b841e8b43bb9f277b9
Vinicius Rocha <vinicius-rochacruz@hotmail.com>
2023-04-09 16:24:13 -0300

    Atualizado front-end chatbot e desenvoldido funções de controle javascript

--- QuickDeveloper/wwwroot/js/ChatBot.js
+++ QuickDeveloper/wwwroot/js/ChatBot.js
@@ -1,19 +1,90 @@
-﻿$(document).keypress(function (e) {
-    if (e.which == 13) $('#Send').click();
+﻿const chatArea = document.getElementById('chat-area');
+const input = document.getElementById('input');
+const sendButton = document.getElementById('send-button');
+const endButton = document.getElementById('endcall-button');
+const button = document.querySelector('.chat-button');
+
+
+/*---------- Adiciona um listener ao evento DOMContentLoaded ----------*/
+document.addEventListener('DOMContentLoaded', iniciarChat); 
+
+/*---------- Evento de clicar no botão encerrar chamada--------*/
+
+endButton.addEventListener('click', function () {
+    const message = "Chamada encerrada"; //armazena em 'message' o valor do textbox
+    input.value = '';//limpa o texbox
+    
+    sendMessage(message, 'bot');
+    input.placeholder = "Chamada encerrada";
+    input.disabled = true;
+    endButton.disabled = true;
+    sendButton.disabled = true;
+
+    setTimeout(() => {//aguarda 0,3segundos
+        sendMessage("Por favor aguarde enquanto carregamos os resultados para você", 'bot');
+    }, 2000);
 });
 
-$(function SendMenssage() {
-    $("#Send").click(
-        function () {
-            var menssage = $("#MenssageTxt").val();
-
-            if (menssage != "") { 
-                $("#Chatbot").append("Eu: " + menssage + "\n")
-                $("#Chatbot").append("ChatBot: " + "Ainda estamos em fase de testes!"+"\n");
-                $("#MenssageTxt").val("");
-            }
-
-        }
-    );
+/*---------- Evento de clicar no botão de enviar --------*/
+sendButton.addEventListener('click', function () {
+    const message = input.value.trim(); //armazena em 'message' o valor do textbox
+    if (message === '') return; // não enviar mensagem vazia
+    input.value = '';//limpa o texbox
+    sendMessage(message, 'user');//chama a funcão passando a menssagem da variavel e o remetente como usuario
+    setTimeout(() => {//aguarda 0,3segundos
+        sendMessage("Sinto muito, ainda não é possivel estabelecer uma comunicação, Aguardes", 'bot'); //chama a funcao com uma mensagem padrao tendo o bot como remetente
+    }, 300);
 });
+
+/*---------- Evento de precionar Enter e enviar --------*/
+input.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {//Avalia se a tecla precisonada é o 'Enter'
+        const message = input.value.trim();//armazena em 'message' o valor do textbox
+        if (message === '') return;// não enviar mensagem vazia
+        input.value = '';//limpa o texbox
+        sendMessage(message, 'user');//chama a funcão passando a menssagem da variavel e o remetente como usuario
+        setTimeout(() => {//aguarda 0,3segundos
+            sendMessage("Sinto muito, ainda não é possivel estabelecer uma comunicação, Aguarde", 'bot');//chama a funcao com uma mensagem padrao tendo o bot como remetente
+        }, 300);
+    }
+});
+
+/* --------Função para carregar mensagens de boas-vindas---------*/
+function iniciarChat() {
+    input.placeholder = "Aguarde um momento";
+    input.disabled = true;
+    endButton.disabled = true;
+    sendButton.disabled = true;
+    sendMessage("Bem-vindo ao nosso chat", 'bot');
+    setTimeout(() => {//aguarda 2 segundos APÓS CHAMADA DA FUNÇÃO
+        sendMessage("Como posso ajuda-lo?", 'bot');
+    }, 2000);
+    setTimeout(() => {//aguarda 2 segundos APÓS CHAMADA DA FUNÇÃO
+        input.disabled = false;
+        endButton.disabled = false;
+        sendButton.disabled = false;
+        input.placeholder = "Digite sua menssagem";
+    }, 3000);
+}
+
+
+
+/* --------Função para enviar mensagens---------*/
+function sendMessage(message, sender) { //recebe a mensagem a postar na tela e quem é o remetente
+    const chatBubble = document.createElement('div'); //constroe um novo elemento "Bolha"
+    chatBubble.classList.add('chat-bubble');//atribui o style da classe 'chat-bubble'
+    chatBubble.innerHTML = message; //atribui a div o texto recebido em message
+    if (message == "Chamada encerrada") {
+        chatBubble.classList.add('endcall');
+    } else {
+        chatBubble.classList.add(sender); //adiciona o style da classe remetente
+    }
+    
+    if (chatArea.children.length === 0) {
+        chatBubble.classList.add('first'); //Adiciona classe 'first' para a primeira mensagem do chat
+    }
+
+    chatArea.appendChild(chatBubble); //Adiciona a nova div criada no final da tela do chat
+    chatArea.scrollTop = chatArea.scrollHeight; //faz a barra de rolagem acompanhar a ultima menssagem digitada
+}
 
