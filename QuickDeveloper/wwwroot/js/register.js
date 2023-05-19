e178fdccb67484d05fc70baa9710251c6d4e8174
Felipe Rodrigues <72510686+feliperodrigs1@users.noreply.github.com>
2023-05-18 21:15:23 -0300

    js para validar requisitos da senha (#25)

--- QuickDeveloper/wwwroot/js/register.js
+++ QuickDeveloper/wwwroot/js/register.js
@@ -3,6 +3,7 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 const form = document.getElementById("signup-form");
+const passwordInput = document.querySelector("#input-password");
 let dev = false;
 
 sign_up_btn.addEventListener("click", () => {
@@ -16,8 +17,8 @@ sign_in_btn.addEventListener("click", () => {
 const dateInput = document.querySelector('#Birthdate');
 
 dateInput.addEventListener('input', (event) => {
-    const input = event.target;
-    const value = input.value.replace(/\D/g, '');
+    let input = event.target;
+    let value = input.value.replace(/\D/g, '');
 
     if (value.length > 8) {
         input.value = value.slice(0, 8);
@@ -30,3 +31,52 @@ dateInput.addEventListener('input', (event) => {
             value.slice(0, 2) + '/' + value.slice(2, 4) + '/' + value.slice(4);
     }
 });
+
+passwordInput.addEventListener('input', () => {
+    let value = passwordInput.value;
+
+    validateUpperCase(value);
+
+    validateLowerCase(value);
+
+    validateNumber(value);
+    
+    validateSymbol(value);
+
+    validateSyze(value);
+});
+
+function validateUpperCase(value) {
+    let upperCase = /[A-Z]/.test(value);
+    let upperTxt = document.querySelector("#upkey");
+
+    upperCase ? upperTxt.classList.add("hidden") : upperTxt.classList.remove("hidden");
+}
+
+function validateLowerCase(value) {
+    let lowerCase = /[a-z]/.test(value);
+    let lowerTxt = document.querySelector("#lowerkey");
+
+    lowerCase ? lowerTxt.classList.add("hidden") : lowerTxt.classList.remove("hidden");
+}
+
+function validateNumber(value) {
+    let hasNumber = /[0-9]/.test(value);
+    let numberTxt = document.querySelector("#number");
+
+    hasNumber ? numberTxt.classList.add("hidden") : numberTxt.classList.remove("hidden");
+}
+
+function validateSymbol(value) {
+    let hasSymbol = /[!@#$%^&+=]/.test(value);
+    let symbolTxt = document.querySelector("#symbol");
+
+    hasSymbol ? symbolTxt.classList.add("hidden") : symbolTxt.classList.remove("hidden");
+}
+
+function validateSyze(value) {
+    let validSize = value.length >= 5;
+    let sizeTxt = document.querySelector("#size");
+
+    validSize ? sizeTxt.classList.add("hidden") : sizeTxt.classList.remove("hidden");
+}
